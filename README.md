21006635ca847bb0288fb0818968b95afa9a3a21
Craig Isesele <hello@craigisesele.com>
2025-11-27 12:47:32 -0500

    rename component\

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 Inspired by [Opta's Football Predictions](https://theanalyst.com/articles/opta-football-predictions) and [this](https://github.com/BelNaruto/English-Premier-League-API) api
 The purpose of this app is to create a place where one can determine where they believe each team in a respective league will end up
 
+Accessibility
+- on tabbing you should be able to tab through the teams
+- when you're on a team, using the up and down arrow keys you can update the team's position
+
+
 Things I want to do:
 
 - Track standings over a season
--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,37 @@ import React from "react";
 import "./App.css";
 import leagues, { type League } from "./db/leagues";
 import Dropdown from "./components/Dropdown";
-import TabList from "./components/TabList";
+import DraggableTabList from "./components/DraggableTabList";
+import { UploadIcon } from "@phosphor-icons/react";
 
 const DEFAULT_LEAGUE = leagues[0];
 function App() {
   const [league, setLeague] = React.useState<League>(DEFAULT_LEAGUE);
   return (
-    <form>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log(league);
+      }}
+    >
       <main>
-        <Dropdown
-          options={leagues.map((league) => league.name)}
-          onChange={(e: React.SyntheticEvent<HTMLSelectElement>) =>
-            setLeague(
-              leagues.find((league) => league.name === e.currentTarget.value) ||
-                DEFAULT_LEAGUE
-            )
-          }
-        />
-        <TabList values={league.teams} />
+        <span>
+          <Dropdown
+            options={leagues.map((league) => league.name)}
+            onChange={(e: React.SyntheticEvent<HTMLSelectElement>) =>
+              setLeague(
+                leagues.find(
+                  (league) => league.name === e.currentTarget.value
+                ) || DEFAULT_LEAGUE
+              )
+            }
+          />
+          <button type="submit">
+            <UploadIcon />
+          </button>
+        </span>
+
+        <DraggableTabList values={league.teams} />
       </main>
     </form>
   );

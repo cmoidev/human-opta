07c4c70ec5b52d14f42f4ea8679c5f92a4e82d49
Craig Isesele <hello@craigisesele.com>
2025-11-27 15:28:44 -0500

    setting up mouse events

--- src/components/DraggableTabList/DraggableTabList.tsx
+++ src/components/DraggableTabList/DraggableTabList.tsx
@@ -7,15 +7,26 @@ function DraggableTabList({ values }: { values: Teams }) {
   const id = React.useId();
   const [selectedTab, setSelectedTab] = React.useState<string | null>(null);
 
+  const selectedTabRef = React.useRef<string | null>(selectedTab);
+
   React.useEffect(() => {
-    function handleMouseDown(e: MouseEvent) {
-      console.log("clicking on tab", selectedTab);
-    }
-    document.addEventListener("mousedown", handleMouseDown);
-    return () => {
-      document.removeEventListener("mousedown", handleMouseDown);
-    };
+    selectedTabRef.current = selectedTab;
+    console.log(selectedTabRef.current);
   }, [selectedTab]);
+
+  React.useEffect(() => {
+    function handleMouseMove(e: MouseEvent) {
+      if (selectedTabRef.current) {
+        console.log("clicking on tab", selectedTabRef.current);
+      }
+    }
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  console.log(selectedTab, "selected tab");
 
   if (Object.keys(values).length === 0) {
     return <div>No teams found</div>;
@@ -37,6 +48,7 @@ function DraggableTabList({ values }: { values: Teams }) {
               name={name}
               position={position ?? index + 1}
               color={color}
+              selectedTab={selectedTab}
               setSelectedTab={setSelectedTab}
             />
           );
@@ -50,17 +62,28 @@ const Tab = ({
   name,
   position,
   color,
+  selectedTab,
   setSelectedTab,
 }: {
   name: string;
   position: number;
   color: string;
+  selectedTab: string | null;
   setSelectedTab: (name: string) => void;
 }) => {
   return (
-    <tr onClick={() => setSelectedTab(name)} className={styles.row}>
+    <tr
+      onMouseDown={() => {
+        setSelectedTab(name);
+      }}
+      className={styles.row}
+    >
       <td>{position}</td>
-      <td className={styles.team}>
+      <td
+        className={`${styles.team} ${
+          selectedTab === name ? styles.selected : ""
+        }`}
+      >
         <SoccerBallIcon color={color} />
         <span>{name}</span>
       </td>
